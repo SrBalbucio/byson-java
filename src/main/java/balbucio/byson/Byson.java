903e43375486f55cb83ff927f67f0d3c6bb8e4ae
SrBalbucio <srbalbuciogf@gmail.com>
2024-05-25 22:18:32 -0300

    Initial commit

---
+++ src/main/java/balbucio/byson/Byson.java
@@ -0,0 +1,20 @@
+package balbucio.byson;
+
+import org.json.JSONObject;
+
+import java.nio.ByteBuffer;
+
+public class Byson {
+
+    public Byson(){
+
+    }
+
+    public JSONObject toJSON(){
+        return null;
+    }
+
+    public ByteBuffer toByteBuffer(){
+        return null;
+    }
+}
